df892d67a712677f11acc93d399c93cf76f279de
Brett Johnston <bjohnston@govsco.net>
2021-03-04 11:41:09 -0700

    Fix Ex006 test code

--- test/exercise006.test.js
+++ test/exercise006.test.js
@@ -1,14 +1,14 @@
 const {
-    findNextNumber
+    areWeCovered
   } = require("../challenges/exercise006");
   
-  describe("createMatrix", () => {
+  describe("areWeCovered", () => {
     test("The function should return true/false depending on whether there are enough staff scheduled for the given day", () => {
       const staff = [{ name: "Sally", rota: ["Monday", "Tuesday", "Friday"] },
                      { name: "Pedro", rota: ["Saturday", "Sunday", "Tuesday", "Wednesday"] },
                      { name: "Susan", rota: ["Saturday", "Tuesday", "Wednesday"] }
                     ]
       const day = "Tuesday"
-      expect(createMatrix(staff, day)).toBe(true);
+      expect(areWeCovered(staff, day)).toBe(true);
     });
   });
